8bb80b67fee491ca681b2aefc18a169a17b526d9
thornicek <tomas.hornicek8@email.cz>
2024-01-26 16:00:13 -0500

    App Finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Tours from "./components/Tours";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
   return ( 
   <>
      <Navbar />
      <Hero />
+     <About />
      <Services/>
      <Tours />
      <Footer />
